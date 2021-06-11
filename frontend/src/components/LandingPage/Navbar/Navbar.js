8756788814ff35d63b5afc6c6cfa914929a28dd9
ayush <ayush.millionaire8750@gmail.com>
2021-06-11 18:11:20 +0530

    added navbar transitions

--- frontend/src/components/LandingPage/Navbar/Navbar.js
+++ frontend/src/components/LandingPage/Navbar/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Gollaa_logo from '../../../assets/images/Gollaa_logo.svg';
-import Menu from '../../../assets/images/Menu.svg';
+// import Menu from '../../../assets/images/Menu.svg';
 
 export default function Navbar(){
 
+    const [open,setOpen] = React.useState(false);
+
+    
     return(
         <React.Fragment>
             <div className="Navbar">
@@ -12,8 +15,19 @@ export default function Navbar(){
                     <img src={Gollaa_logo} alt="Golla_logo" />
                 </div>
 
-                <div className="Navbar-menu">
-                    <img src={Menu} alt="Menu_toggler" />
+                <div  className="Navbar-menu">
+                    {/* <img src={Menu} alt="Menu_toggler" /> */}
+                    <div onClick={()=>setOpen(prevState=>!prevState)} className='Navbar-menu-box'>
+                        <div className={open ? 'Navbar-menu-box-open-line2' : 'Navbar-menu-box-line2'}></div>
+                    </div>
+
+                    <div className={open ? "Navbar-menu-options" : "Navbar-menu-none"}>
+                        <ul className={open ? "Navbar-menu-options-list" : "Navbar-menu-none-list"}>
+                            <li>Blog</li>
+                            <li>Our Stories</li>
+                            <li>About Us</li>
+                        </ul>
+                    </div>
                 </div>
                 
             </div>
