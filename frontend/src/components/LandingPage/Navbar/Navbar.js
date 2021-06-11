f49c4b2760e6d5fc6abd7222fcf57e2a1004873c
ayush <ayush.millionaire8750@gmail.com>
2021-06-11 18:25:36 +0530

    navbar toggling bug solved with useRed

--- frontend/src/components/LandingPage/Navbar/Navbar.js
+++ frontend/src/components/LandingPage/Navbar/Navbar.js
@@ -6,7 +6,21 @@ export default function Navbar(){
 
     const [open,setOpen] = React.useState(false);
 
+    React.useEffect(() => {
+        let handler = (event) => {
+          if (!navNode.current.contains(event.target) && !toggler.current.contains(event.target)) {
+            setOpen(false);
+          }
+        };
+        document.addEventListener("mousedown", handler);
+        return () => {
+          document.removeEventListener("mousedown", handler);
+        };
+      }, []);
     
+      let navNode = React.useRef();
+      let toggler = React.useRef();
+
     return(
         <React.Fragment>
             <div className="Navbar">
@@ -17,11 +31,11 @@ export default function Navbar(){
 
                 <div  className="Navbar-menu">
                     {/* <img src={Menu} alt="Menu_toggler" /> */}
-                    <div onClick={()=>setOpen(prevState=>!prevState)} className='Navbar-menu-box'>
+                    <div ref={toggler} onClick={()=>setOpen(prevState=>!prevState)} className='Navbar-menu-box'>
                         <div className={open ? 'Navbar-menu-box-open-line2' : 'Navbar-menu-box-line2'}></div>
                     </div>
 
-                    <div className={open ? "Navbar-menu-options" : "Navbar-menu-none"}>
+                    <div ref={navNode} className={open ? "Navbar-menu-options" : "Navbar-menu-none"}>
                         <ul className={open ? "Navbar-menu-options-list" : "Navbar-menu-none-list"}>
                             <li>Blog</li>
                             <li>Our Stories</li>
