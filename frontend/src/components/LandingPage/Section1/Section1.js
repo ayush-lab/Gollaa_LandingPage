d6478ca77448a916cf608f977278f65bc7aa30fd
ayush <ayush.millionaire8750@gmail.com>
2021-07-23 19:55:29 +0530

    responsiveness and overlays improved

--- frontend/src/components/LandingPage/Section1/Section1.js
+++ frontend/src/components/LandingPage/Section1/Section1.js
@@ -20,7 +20,10 @@ export default function Section1(){
 
 
   const shopFullScreen=()=>{
-    var windowWidth = window.matchMedia("(max-width: 900px)")
+    var windowWidth = window.matchMedia("(max-width: 900px)") 
+    if(!windowWidth.matches){
+        windowWidth = window.matchMedia("(max-height: 600px)") 
+    }
     if(windowWidth.matches){
         shopHandler(prevState=>({...prevState,shopModal:true}));
     }
@@ -31,6 +34,9 @@ export default function Section1(){
 
   const growFullScreen=()=>{
     var windowWidth = window.matchMedia("(max-width: 900px)")
+    if(!windowWidth.matches){
+        windowWidth = window.matchMedia("(max-height: 600px)") 
+    }
     if(windowWidth.matches){
         growHandler(prevState=>({...prevState,growModal:true}));
     }
@@ -41,6 +47,9 @@ export default function Section1(){
 
   const supportFullScreen=()=>{
     var windowWidth = window.matchMedia("(max-width: 900px)")
+    if(!windowWidth.matches){
+        windowWidth = window.matchMedia("(max-height: 600px)") 
+    }
     if(windowWidth.matches){
         supportHandler(prevState=>({...prevState,supportModal:true}));
     }
@@ -50,6 +59,9 @@ export default function Section1(){
 
   const stayFullScreen=()=>{
     var windowWidth = window.matchMedia("(max-width: 900px)")
+    if(!windowWidth.matches){
+        windowWidth = window.matchMedia("(max-height: 600px)") 
+    }
     if(windowWidth.matches){
         stayHandler(prevState=>({...prevState,stayModal:true}));
     }
@@ -59,6 +71,9 @@ export default function Section1(){
 
   const tradeFullScreen=()=>{
     var windowWidth = window.matchMedia("(max-width: 900px)")
+    if(!windowWidth.matches){
+        windowWidth = window.matchMedia("(max-height: 600px)") 
+    }
     if(windowWidth.matches){
         tradeHandler(prevState=>({...prevState,tradeModal:true}));
     }
@@ -177,6 +192,24 @@ export default function Section1(){
     },1500)
   }
 
+  React.useEffect(() => {
+    window.addEventListener("resize", handleResize)
+
+    return ()=>{
+        window.removeEventListener('resize')
+    }
+  },[])
+
+   const handleResize = ()=>{
+  
+    shopHandler(prevState=>({...prevState,shopOpen:false,shopClicked:false,shopClosedClick:false}))
+    growHandler(prevState=>({...prevState,growOpen:false,growClicked:false,growClosedClick:false}))
+    stayHandler(prevState=>({...prevState,stayOpen:false,stayClicked:false,stayClosedClick:false}))
+    supportHandler(prevState=>({...prevState,supportOpen:false,supportClicked:false,supportClosedClick:false}))
+    tradeHandler(prevState=>({...prevState,tradeOpen:false,tradeClicked:false,tradeClosedClick:false}))
+  
+}
+
   let shopRef=useRef();
   let shopFullScreenText=useRef();
 
@@ -212,12 +245,12 @@ export default function Section1(){
       overlayShop=["overlay-motion"]
   }
   if(shop.shopOpen){
-    // document.querySelector("body").style.overflow = "hidden";
+    document.querySelector("html").style.overflow = "hidden";
   }
   
   if(shop.shopClosedClick){
     overlayShop=["overlay-come"]
-    // document.querySelector("body").style.overflow = "auto";
+    document.querySelector("html").style.overflow = "auto";
   }
 
     // grow
@@ -232,11 +265,11 @@ export default function Section1(){
        overlayGrow=["overlay-motion"]
    }
    if(grow.growOpen){
-   // document.querySelector("body").style.overflow = "hidden";
+   document.querySelector("html").style.overflow = "hidden";
    }     
    if(grow.growClosedClick){
    overlayGrow=["overlay-come"]
-   // document.querySelector("body").style.overflow = "auto";
+   document.querySelector("html").style.overflow = "auto";
    }     
    
    // support
@@ -251,11 +284,11 @@ export default function Section1(){
         overlaySupport=["overlay-motion"]
     }
     if(support.supportOpen){
-    // document.querySelector("body").style.overflow = "hidden";
+    document.querySelector("html").style.overflow = "hidden";
     }     
     if(support.supportClosedClick){
-    overlaySupport=["overlay-come"]
-    // document.querySelector("body").style.overflow = "auto";
+        overlaySupport=["overlay-come"]
+        document.querySelector("html").style.overflow = "auto";
     }  
 
 
@@ -271,11 +304,11 @@ export default function Section1(){
         overlayStay=["overlay-motion"]
     }
     if(stay.stayOpen){
-    // document.querySelector("body").style.overflow = "hidden";
+        document.querySelector("html").style.overflow = "hidden";
     }     
     if(stay.stayClosedClick){
-    overlayStay=["overlay-come"]
-    // document.querySelector("body").style.overflow = "auto";
+        overlayStay=["overlay-come"]
+        document.querySelector("html").style.overflow = "auto";
     }  
 
      // Trade
@@ -290,11 +323,11 @@ export default function Section1(){
         overlayTrade=["overlay-motion"]
     }
     if(trade.tradeOpen){
-    // document.querySelector("body").style.overflow = "hidden";
+        document.querySelector("html").style.overflow = "hidden";
     }     
     if(trade.tradeClosedClick){
-    overlayTrade=["overlay-come"]
-    // document.querySelector("body").style.overflow = "auto";
+        overlayTrade=["overlay-come"]
+        document.querySelector("html").style.overflow = "auto";
     }  
 
 
